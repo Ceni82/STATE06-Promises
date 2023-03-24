8c6a7d384a5bca4bf1071e1a78189970aac340fc
Mateus Ceni <mateus.ceni@gmail.com>
2023-03-23 21:45:25 -0300

    por hoje é só pessoal, + amanhã (sem erros =)

--- js/favorites.js
+++ js/favorites.js
@@ -1,29 +1,37 @@
+export class GithubUser {
+  static search(username) {
+    const endpoint = `https://api.github.com/users/${username}`
+
+    return fetch(endpoint)
+    .then (data =>data.json())
+    .then (({login, name, public_repos, followers}) => ({
+      login, name, public_repos, followers
+    }))
+  }
+}
+
+
 
 export class Favorites {
     constructor (root) {
         this.root = document.querySelector(root)
         this.load()
+
+        GithubUser.search('Ceni82').then(user => console.log(user))
     }
 
     load() {
-        this.entries =[
-            {
-                login: 'maykbrito',
-                name: 'Mayk Brito',
-                public_repos:'76',
-                followers: '12000'
-            },
-            {
-                login: 'maykbrito',
-                name: 'Mayk Brito',
-                public_repos:'76',
-                followers: '12000'
-            }
-        ]
+        this.entries = JSON.parse(localStorage.getItem
+          ('@github-favorites:')) || []
+ 
+    }
+    delete(user){
+      const filteredEntries = this.entries
+      .filter(entry => entry.login !== user.login)
 
-       
-       
-    
+      this.entries = filteredEntries
+      this.update() 
+      
     }
 }
 
@@ -31,9 +39,9 @@ export class FavoritesView extends Favorites {
     constructor (root){
         super(root)
 
-        thid.tbody = this.root.querySelector('table tbody');
+        this.tbody = this.root.querySelector('table tbody')
 
-      this.update()
+        this.update()
     }
 
     createRow() {
@@ -59,24 +67,7 @@ export class FavoritesView extends Favorites {
           </td>
         </tr>
 
-        <tr>
-          <td class="user">
-            <img src="https://github.com/diego3g.png" alt="Imagem de maykbrito">
-            <a href="https://github.com/diego3g" target="_blank">
-              <p>Diego Fernandes</p>
-              <span>diego3g</span>
-            </a>
-          </td>
-          <td class="repositories">
-            48
-          </td>
-          <td class="followers">
-            22503
-          </td>
-          <td>
-            <button class="remove">&times;</button>
-          </td>
-      </tr>
+       
         `
 
         return tr
@@ -97,9 +88,14 @@ export class FavoritesView extends Favorites {
             row.querySelector('.repositories').textContent = user.public_repos
             row.querySelector('.followers').textContent = user.followers
 
+            row.querySelector('.remove').onclick = () => {
+    
+              const isOk = confirm (' Tem certeza que deseja deletar?')
+              if(isOk){
+                this.delete(user)
+              }
+            }
 
-            
-            
          this.tbody.append(row)
     })
 
--- js/main.js
+++ js/main.js
@@ -1,3 +1,3 @@
-import { FavoritesView } from "./favor.js";
+import { FavoritesView } from "./favorites.js";
 
 new FavoritesView ('#app')
